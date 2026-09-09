ad78e8585fdb162ba4f0f16426b773c7c381ea2a
bugnotfeature <jasmina.bichmann@gmail.com>
2026-09-09 10:55:42 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -227,7 +227,7 @@ function drawQuestion() {
         <div class="prompt">Welches Kapitel gehört zu diesem Kapitelnamen?</div>
         <div class="question">${current[1]}</div>
         <div class="answer-row">
-            <input class="answer" id="answer" inputmode="decimal" enterkeyhint="done" autocomplete="off" autocapitalize="off" autocorrect="off" spellcheck="false">
+            <input class="answer" id="answer" inputmode="text" enterkeyhint="done" autocomplete="off" autocapitalize="off" autocorrect="off" spellcheck="false">
             <div id="mobileActionSlot"></div>
         </div>
         <div class="feedback" id="feedback"></div>
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "finding-normo-v4";
+const CACHE_NAME = "finding-normo-v5";
 const APP_FILES = [
   "./",
   "./index.html",
