6634320954b09492b1207bd09eccbc7c10f9f46a
bugnotfeature <jasmina.bichmann@gmail.com>
2026-09-09 15:54:13 +0200

    Update game.js

--- game.js
+++ game.js
@@ -246,7 +246,7 @@ function drawQuestion() {
     `;
 
     const input = document.getElementById("answer");
-    const mobileAction = btn(" OK ", () => {
+    const mobileAction = btn(" Okay ", () => {
         if (waiting) {
             next();
         } else {
